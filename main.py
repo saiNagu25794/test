34e821ebf876827356364cb693061b8e5901a363
saiNagu25794 <sai25794@gmail.com>
2023-01-10 16:53:15 +0530

    first flask commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import uvicorn
+from fastapi import FastAPI, Body
+from typing import Any
+app = FastAPI()
+
+
+@app.get("/index")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/employee")
+def employee(employee_id: int, employee_name: str, employee_salary=None):
+    #
+    return {"employee_id": employee_id, "employee_name": employee_name, "employee_salary": employee_salary}
+
+@app.post("/employee/data")
+def employee_create(data: Any = Body(...)):
+    # response = {
+    #     "status": 404,
+    #     "message": "employee not found"
+    # }
+    return data
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8080)
